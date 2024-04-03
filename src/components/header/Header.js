ee64a7dcd1b401cec00eb81f837253e27de78419
hensell de la rosa <hensell1234@gmail.com>
2024-04-03 19:49:30 -0400

    header done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Header.scss'
+import {Person, Forum, IButton} from '../../exports'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <IButton>
+        <Person className='header__icon' fontSize='large'/>
+      </IButton>
+
+      <IButton>
+        <img
+        src="img/tinder-logo.svg"
+        alt="tinder logo "
+        className="header__logo"
+        />
+      </IButton>
+
+      <IButton>
+        <Forum className='header__icon' fontSize='large'/>
+      </IButton>
+    </div>
   )
 }
 
--- src/exports.js
+++ src/exports.js
@@ -9,6 +9,18 @@ export {default as Header} from './components/header/Header'
 // export {default as } from './components//'
 // export {default as } from './components//'
 
+// Icons
+export {default as Person} from '@mui/icons-material/Person'
+export {default as Forum} from '@mui/icons-material/Forum'
+export {default as IButton} from '@mui/material/IconButton'
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+// export {default as } from '@mui/icons-material/
+
 // pages
 // export {default as } from './pages//'
 // export {default as } from './pages//'
